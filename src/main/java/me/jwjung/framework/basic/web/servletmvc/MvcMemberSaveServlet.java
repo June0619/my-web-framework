a859b67993d4b120dd898d1351f262c17be0a4c5
June0619 <jjw5050@hotmail.com>
2023-10-24 13:37:26 +0900

    MVC 패턴 적용

---
+++ src/main/java/me/jwjung/framework/basic/web/servletmvc/MvcMemberSaveServlet.java
@@ -0,0 +1,36 @@
+package me.jwjung.framework.basic.web.servletmvc;
+
+import me.jwjung.framework.basic.domain.member.Member;
+import me.jwjung.framework.basic.domain.member.MemberRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
+public class MvcMemberSaveServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        System.out.println("member = " + member);
+        memberRepository.save(member);
+
+        //Model 에 저장된 데이터 저장
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher("viewPath");
+        dispatcher.forward(request, response);
+    }
+}
