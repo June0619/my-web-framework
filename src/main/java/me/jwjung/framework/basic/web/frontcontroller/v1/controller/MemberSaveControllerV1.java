966c8f95a07ee55ea525767c7cfaa784144cf6f5
June0619 <jjw5050@hotmail.com>
2024-04-02 16:24:45 +0900

    wip: my interface v1

---
+++ src/main/java/me/jwjung/framework/basic/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -0,0 +1,15 @@
+package me.jwjung.framework.basic.web.frontcontroller.v1.controller;
+
+import me.jwjung.framework.basic.web.frontcontroller.v1.ControllerV1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
